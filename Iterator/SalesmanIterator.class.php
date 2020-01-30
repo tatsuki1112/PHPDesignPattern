f11f22e4394f16928d499ea7ebe678984c8a6b00
tatsuki1112@gmail.com <tatsuki1112@gmail.com>
2020-01-30 20:57:50 +0900

    Iterator

---
+++ Iterator/SalesmanIterator.class.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Employee.class.php';
+
+class SalesmanIterator extends FilterIterator
+{
+    public function __construct($iterator)
+    {
+        parent::__construct($iterator);
+    }
+
+    public function accept()
+    {
+        $employee = $this->getInnerIterator()->current();
+        return($employee->getJob() === 'SALESMAN');
+    }
+}
